0bb96d3e936f015f3d2f4eb0f2cbbd7d883d4b6e
bumpy09 <101124139+bumpy09@users.noreply.github.com>
2022-07-11 10:52:57 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+// Send all received packets to serial output
+radio.onReceivedNumber(function (receivedNumber) {
+    radio.writeReceivedPacketToSerial()
+    led.toggle(randint(0, 4), randint(0, 4))
+})
+// Decrement radio group by 1
+input.onButtonPressed(Button.A, function () {
+    group = Math.max(0, group - 1)
+    radio.setGroup(group)
+    led.stopAnimation()
+    basic.showNumber(group)
+})
+// Increment radio group by 1
+input.onButtonPressed(Button.B, function () {
+    group = Math.min(255, group + 1)
+    radio.setGroup(group)
+    led.stopAnimation()
+    basic.showNumber(group)
+})
+let group = 0
+group = 128
+radio.setGroup(group)
